32b9b2869f6585b743f625a7e3999495ee96bded
Iori <iorilima13@gmail.com>
2026-05-11 19:52:38 -0300

    add

---
+++ Prot1/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Prot1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnConsut = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.cbRaca = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbGenero = new System.Windows.Forms.ComboBox();
+            this.btnConsutList = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Raça";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(29, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "pelagem";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(407, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Idade";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(52, 187);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(200, 39);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Salvae";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(52, 233);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(200, 39);
+            this.btnEdit.TabIndex = 7;
+            this.btnEdit.Text = "Alterar";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnConsut
+            // 
+            this.btnConsut.Location = new System.Drawing.Point(52, 279);
+            this.btnConsut.Name = "btnConsut";
+            this.btnConsut.Size = new System.Drawing.Size(200, 39);
+            this.btnConsut.TabIndex = 8;
+            this.btnConsut.Text = "Consultar";
+            this.btnConsut.UseVisualStyleBackColor = true;
+            this.btnConsut.Click += new System.EventHandler(this.btnConsut_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(52, 324);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(200, 39);
+            this.btnExcluir.TabIndex = 9;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(360, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(428, 426);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(90, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 22);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(90, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(145, 22);
+            this.textBox2.TabIndex = 12;
+            // 
+            // cbRaca
+            // 
+            this.cbRaca.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbRaca.FormattingEnabled = true;
+            this.cbRaca.Location = new System.Drawing.Point(90, 84);
+            this.cbRaca.Name = "cbRaca";
+            this.cbRaca.Size = new System.Drawing.Size(145, 24);
+            this.cbRaca.TabIndex = 13;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(90, 114);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(145, 24);
+            this.comboBox2.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(29, 147);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 16);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Genero";
+            // 
+            // cbGenero
+            // 
+            this.cbGenero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbGenero.FormattingEnabled = true;
+            this.cbGenero.Location = new System.Drawing.Point(90, 144);
+            this.cbGenero.Name = "cbGenero";
+            this.cbGenero.Size = new System.Drawing.Size(145, 24);
+            this.cbGenero.TabIndex = 16;
+            // 
+            // btnConsutList
+            // 
+            this.btnConsutList.Location = new System.Drawing.Point(52, 369);
+            this.btnConsutList.Name = "btnConsutList";
+            this.btnConsutList.Size = new System.Drawing.Size(200, 39);
+            this.btnConsutList.TabIndex = 17;
+            this.btnConsutList.Text = "Consultar Lista";
+            this.btnConsutList.UseVisualStyleBackColor = true;
+            this.btnConsutList.Click += new System.EventHandler(this.btnConsutList_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConsutList);
+            this.Controls.Add(this.cbGenero);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.cbRaca);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnConsut);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnConsut;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox cbRaca;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbGenero;
+        private System.Windows.Forms.Button btnConsutList;
+    }
+}
+
---
+++ Prot1/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+
+namespace Prot1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string DADOS_CONEXAO = "server=localhost;user=root;password=;database=;";
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text;
+            string idade = txtIdade.Text;
+            string raca = cbRaca.Text;
+            string pelagem = cbPelagem.Text;
+            string gene = cbGenero.Text;
+
+            MessageBox.Show($"O nome do gato é {nome}, da raça {raca}, com a pelagem {pelagem} foi salvo com {idade} anos deidade. O gato é {gene}");
+
+
+
+            int controleLinhasAfetadas = 0;
+
+
+            using (MySqlConnection conn = new MySqlConnection(DADOS_CONEXAO))
+            {
+                conn.Open();
+                string scriptInsert = "INSERT INTO tb_gatos (nome) VALUE (@nome)";
+
+                using (MySqlCommand comando = new MySqlCommand(scriptInsert, conn))
+                {
+                    comando.Parameters.AddWithValue("@nome", nome);
+                    comando.Parameters.AddWithValue("@idade", idade);
+                    comando.Parameters.AddWithValue("@raca", raca);
+                    comando.Parameters.AddWithValue("@pelagem", pelagem);
+                    comando.Parameters.AddWithValue("@gene", gene);
+                    controleLinhasAfetadas = comando.ExecuteNonQuery();
+
+                    comando.ExecuteNonQuery();
+                }
+            }
+            //MysqlConnection
+
+            if (controleLinhasAfetadas > 0)
+            {
+                MessageBox.Show("Gato cadastrado com sucesso!");
+            }
+            else
+            {
+                MessageBox.Show("Ops. parece que os novelos estão bagunçados!");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConsut_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(DADOS_CONEXAO))
+            {
+                conn.Open();
+                string scriptConsultaIndividual = "SELECT * FROM tb_gatos WHERE id = @id";
+                using (MySqlCommand comando = new MySqlCommand(scriptConsultaIndividual, conn))
+                {
+                    MySqlDataReader reader = comando.ExecuteReader();
+                    var dadosResultado = comando.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        lbIdResultado.Tesxt = dadosResultado["id"].ToString();
+                        lbNomeResultado.Text = dadosResultado["nome"].ToString();
+                        lbRacaResultado.Text = dadosResultado["raca"].ToString();
+                        lbPelageResultado.Text = dadosResultado["pelagem"].ToString();
+                        lbGeneResultado.Text = dadosResultado["gene"].ToString();
+                    }
+                }
+
+
+
+
+
+
+
+                conn.Close();
+            }
+        }
+
+        private void btnConsutList_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(DADOS_CONEXAO))
+            {
+                conn.Open();
+                string campoPelagem = cbPelagem.Text;
+                string scriptConsultaLista = "SELECT * FROM tb_gatos";
+                    using (MySqlCommand comando = new MySqlCommand(scriptConsultaLista, conn))
+                    {
+                        MySqlDataAdapiter resultadoConsultaMySql = new MySqlDataAdapiter(comando);
+                        
+                        DataTable dt = new DataTable();
+                        resultadoConsultaMySql.Fill(dt);
+                        dgvListarTudo.DataSource = dt;
+                        
+                        
+                    }
+                conn.Close();
+            }
+        }
+    }
+}
